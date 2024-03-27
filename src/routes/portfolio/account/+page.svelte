3ef08b511c664d42c74de8c80e51446e8b6a718f
Seungguen Cho <48639154+sgn-cho@users.noreply.github.com>
2024-03-27 14:57:52 +0900

    ✨feat: 계좌 조회 페이지 구현

---
+++ src/routes/portfolio/account/+page.svelte
@@ -0,0 +1,56 @@
+<div class="container">
+    <h1 class="main-content-large">계좌</h1>
+    <div/>
+</div>
+<div class="container space-y-4 md:space-y-36">
+    <div class="grid grid-cols-2 pt-4 pb-12 space-y-4 md:space-y-0">
+        <h2 class="main-content-small col-span-2 md:col-span-1">ISA</h2>
+        <div class="main-content-small space-y-4 col-span-2 md:col-span-1">
+            <div class="grid grid-cols-2">
+                <div>
+                    <h4>KODEX 선진국MSCI World</h4>
+                    <p>1,090주</p>
+                </div>
+                <div class="text-right">
+                    <p class="font-semibold">27,086,100원</p>
+                    <p class="font-semibold text-red-600">+4,348,775 (19.13%)</p>
+                </div>
+            </div>
+            <div class="grid grid-cols-2">
+                <div>
+                    <h4>VT</h4>
+                    <p>52주</p>
+                </div>
+                <div class="text-right">
+                    <p class="font-semibold">120,090원</p>
+                    <p class="font-semibold text-blue-600">-1.2%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 pt-4 pb-12 space-y-4 md:space-y-0">
+        <h2 class="main-content-small col-span-2 md:col-span-1">종합 계좌</h2>
+        <div class="main-content-small space-y-4 col-span-2 md:col-span-1">
+            <div class="grid grid-cols-2">
+                <div>
+                    <h4>KODEX 선진국MSCI World</h4>
+                    <p>1,090주</p>
+                </div>
+                <div class="text-right">
+                    <p class="font-semibold">120,090원</p>
+                    <p class="font-semibold text-red-600">+1.2%</p>
+                </div>
+            </div>
+            <div class="grid grid-cols-2">
+                <div>
+                    <h4>VT</h4>
+                    <p>52주</p>
+                </div>
+                <div class="text-right">
+                    <p class="font-semibold">120,090원</p>
+                    <p class="font-semibold text-blue-600">-1.2%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
