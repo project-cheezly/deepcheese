a9a27141a142d41697fa7da36ad4d0ee17d8b086
Seungguen Cho <48639154+sgn-cho@users.noreply.github.com>
2024-04-15 21:40:48 +0900

    bugfix: candle chart 버그 수정

--- front/src/routes/portfolio/analysis/+page.svelte
+++ front/src/routes/portfolio/analysis/+page.svelte
@@ -14,7 +14,70 @@
         return () => {
             clearInterval(interval);
         }
-    })
+    });
+
+    let categoryValueHistory = [];
+    $: {
+        let temp = $page.data.categoryValueHistory;
+        temp.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+
+        let test = temp.reduce((acc, cur) => {
+            if (!acc.has(cur.category_name)) {
+                acc.set(cur.category_name, []);
+            } else {
+                acc.get(cur.category_name).push(cur);
+            }
+
+            return acc;
+        }, new Map());
+
+        let categoryProperties = new Map();
+        test.forEach((_, k) => {
+            categoryProperties.set(k, {
+                prevValue: 0,
+                inserted: false,
+                closeDate: test.get(k)[test.get(k).length - 1].timestamp.toString()
+            });
+        });
+
+        let prevDate = temp[0].timestamp.toString();
+        let test2 = temp.reduce((acc, cur) => {
+            let currDate = cur.timestamp.toString();
+
+            if (currDate !== prevDate) {
+                categoryProperties.forEach((v, k) => {
+                    if (new Date(prevDate).getTime() < new Date(v.closeDate).getTime() && !v.inserted) {
+                        let prevAccValue = acc.get(prevDate);
+                        acc.set(prevDate, prevAccValue + v.prevValue);
+                    }
+                    categoryProperties.get(k).inserted = false;
+                })
+
+                prevDate = currDate;
+            }
+
+            categoryProperties.get(cur.category_name).inserted = true;
+
+            if (!acc.has(currDate)) {
+                acc.set(currDate, parseFloat(cur.value));
+            } else {
+                let prev = acc.get(currDate);
+                acc.set(currDate, prev + parseFloat(cur.value));
+            }
+
+            categoryProperties.get(cur.category_name).prevValue = parseFloat(cur.value);
+
+            return acc;
+        }, new Map());
+
+        test2.forEach((v, k) => {
+            categoryValueHistory.push({
+                timestamp: new Date(k),
+                category_name: "total",
+                value: v
+            });
+        });
+    }
 </script>
 
 <div class="container">
@@ -30,7 +93,7 @@
         </div>
         <div class="main-content col-span-2 md:col-span-1">
             <h2>일간</h2>
-            <BaseChart data={$page.data.categoryValueHistory} timeScale="date" />
+            <BaseChart data={categoryValueHistory} timeScale="date" />
         </div>
     </Container.Content>
 </Container.Root>
